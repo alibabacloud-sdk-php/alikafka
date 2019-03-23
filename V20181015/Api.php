d7065b333697c806f6c0772bfa96a1fc1f082f79
顾咏 <zhijiang.nzj@alibaba-inc.com>
2019-03-23 17:03:28 +0800

    Update Apis

--- V20181015/Api.php
+++ V20181015/Api.php
@@ -2,7 +2,24 @@
 
 namespace AlibabaCloud\Alikafka\V20181015;
 
+use AlibabaCloud\ApiResolverTrait;
 use AlibabaCloud\Rpc;
+
+/**
+ * Resolve Api based on the method name.
+ *
+ * @method CreateConsumerGroup createConsumerGroup(array $options = [])
+ * @method CreateTopic createTopic(array $options = [])
+ * @method GetTopicStatus getTopicStatus(array $options = [])
+ * @method GetConsumerProgress getConsumerProgress(array $options = [])
+ * @method GetConsumerList getConsumerList(array $options = [])
+ * @method GetInstanceList getInstanceList(array $options = [])
+ * @method GetTopicList getTopicList(array $options = [])
+ */
+class AlikafkaApiResolver
+{
+    use ApiResolverTrait;
+}
 
 class V20181015Rpc extends Rpc
 {
